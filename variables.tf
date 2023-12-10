16eef834b9f7c8f6960f03080e827fc95e558a6f
ajay291491 <ajay291491@gmail.com>
2023-12-10 23:27:48 +0000

    Updated syntax for variables

--- variables.tf
+++ variables.tf
@@ -1,11 +1,7 @@
-variable "ARM_SUBSCRIPTION_ID" {
-}
+variable "ARM_SUBSCRIPTION_ID" {}
 
-variable "ARM_TENANT_ID" {
-}
+variable "ARM_TENANT_ID" {}
 
-variable "ARM_CLIENT_ID" {
-}
+variable "ARM_CLIENT_ID" {}
 
-variable "ARM_CLIENT_SECRET" {
-}
+variable "ARM_CLIENT_SECRET" {}
